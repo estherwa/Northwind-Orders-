d2599e904d3e9e3f7b9115ddbf8fea65ca8ba204
estherwa <estherwa@hotmail.es>
2024-09-22 21:09:40 +0300

    work progress

--- Backend/NorthwindAPI/obj/Debug/net8.0/NorthwindAPI.AssemblyInfo.cs
+++ Backend/NorthwindAPI/obj/Debug/net8.0/NorthwindAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthwindAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcf13e3259b95a40bb8d28cc081912dbc85a87f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31fc72a0cc7ea675c19442c0b9701d9ec75ba2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthwindAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthwindAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
